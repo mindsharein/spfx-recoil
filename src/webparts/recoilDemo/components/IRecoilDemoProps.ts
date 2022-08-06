176f7c472b15c01a8a2ee905e6de030d31fb1a98
Vijay Natrajan <mindshare.learn@gmail.com>
2022-08-06 15:06:51 +0530

    Created spfx-recoil project

---
+++ src/webparts/recoilDemo/components/IRecoilDemoProps.ts
@@ -0,0 +1,7 @@
+export interface IRecoilDemoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/recoilDemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IRecoilDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'RecoilDemoWebPartStrings' {
+  const strings: IRecoilDemoWebPartStrings;
+  export = strings;
+}
